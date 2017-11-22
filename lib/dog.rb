5c71313d1b007fe3d0a31579747d3d8ecb51cf7a
Heather Marie Haas <osxgirl@mac.com>
2017-11-22 01:47:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_reader :name
   attr_accessor :breed
 
-  def initialize(name,breed)
+  def initialize(name,breed = "Mutt")
     puts "A Dog's Name & Breed"
     @name = name
     @breed = breed
